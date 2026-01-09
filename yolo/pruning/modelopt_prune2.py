226541aa788dd6a10cf84e06c2abba15a7fc9089
filiperobotic <filipe.rolim@gmail.com>
2026-01-09 15:30:48 -0300

    wrewrwew

---
+++ yolo/pruning/modelopt_prune2.py
@@ -0,0 +1,82 @@
+from ultralytics import YOLO
+import modelopt.torch.prune as mtp
+
+model = YOLO("yolo11x.pt")
+
+class PrunedTrainer(model.task_map[model.task]["trainer"]):
+  def _setup_train(self):
+    """Modified setup model that adds distillation wrapper."""
+    from ultralytics.utils import LOGGER
+    from ultralytics.utils.torch_utils import ModelEMA
+    from ultralytics import YOLO
+    from collections import OrderedDict
+    import torch
+    import math
+
+    super()._setup_train()
+
+    def collect_func(batch):
+        return self.preprocess_batch(batch)["img"]
+
+    def score_func(model):
+        # Disable logs
+        model.eval()
+        self.validator.args.save = False
+        self.validator.args.plots = False
+        self.validator.args.verbose = False
+        self.validator.args.data = "coco128.yaml"
+        metrics = self.validator(model=model)
+        self.validator.args.save = self.args.save
+        self.validator.args.plots = self.args.plots
+        self.validator.args.verbose = self.args.verbose
+        self.validator.args.data = self.args.data
+        return metrics["fitness"]
+
+    prune_constraints = {"flops": "66%"}  # prune to 66% of original FLOPs
+
+    self.model.is_fused = lambda: True  # disable fusing
+
+    self.model, prune_res = mtp.prune(
+        model=self.model,
+        mode="fastnas",
+        constraints=prune_constraints,
+        dummy_input=torch.randn(1, 3, self.args.imgsz, self.args.imgsz).to(self.device),
+        config={
+            "score_func": score_func,  # scoring function
+            "checkpoint": "modelopt_fastnas_search_checkpoint.pth",  # saves checkpoint during subnet search
+            "data_loader": self.train_loader,  # training dataloader
+            "collect_func": collect_func,  # preprocessing function
+            "max_iter_data_loader": 20,  # 50 is recommended, but requires more RAM
+        },
+    )
+
+    self.model.to(self.device)
+    # Recreate EMA
+    self.ema = ModelEMA(self.model)
+
+    # Recreate optimizer and scheduler
+    weight_decay = self.args.weight_decay * self.batch_size * self.accumulate / self.args.nbs  # scale weight_decay
+    iterations = math.ceil(len(self.train_loader.dataset) / max(self.batch_size, self.args.nbs)) * self.epochs
+    self.optimizer = self.build_optimizer(
+    model=self.model,
+    name=self.args.optimizer,
+    lr=self.args.lr0,
+    momentum=self.args.momentum,
+    decay=weight_decay,
+    iterations=iterations,
+    )
+    self._setup_scheduler()
+    LOGGER.info("Applied pruning")
+
+
+results = model.train(data="./data.yaml", trainer=PrunedTrainer, epochs=50, exist_ok=True, warmup_epochs=0)
+
+pruned_model = YOLO("runs/detect/train/weights/best.pt")
+
+# Original FLOPs
+model = YOLO("yolov11x.pt")
+model.info()
+
+pruned_model.info()
+
+results = pruned_model.val(data="./data.yaml", verbose=False, rect=False)
